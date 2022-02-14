bc24a322ce186041d54235d76a0476e0c3f106a1
Glenn Clapp <40251433+gmclapp@users.noreply.github.com>
2022-02-14 09:29:45 -0500

    More test code

--- Test code.py
+++ Test code.py
@@ -1,10 +1,18 @@
 import pandas as pd
 import pandas_datareader.data as web
 import datetime as dt
+import readchar
 
 ticker = "TSLA"
 source = "yahoo-dividends"
 today = dt.date.today()
 
-df = web.DataReader(ticker,source,today)
-print(df.head())
+try:
+    df = web.DataReader(ticker,source,today)
+    print(df.head())
+except KeyError as err:
+    print("{}, market may not be open.".format(err))
+    
+while True:
+    keypress = readchar.readkey()
+    print(keypress)
