2e89936ceb50aac5e176f35664327ef8e9dc6d6c
Glenn Clapp <40251433+gmclapp@users.noreply.github.com>
2022-11-16 11:20:07 -0500

    Transactions

--- Test code.py
+++ Test code.py
@@ -2,13 +2,16 @@ import pandas as pd
 import pandas_datareader.data as web
 import datetime as dt
 import readchar
+import requests
 
+session=requests.Session()
+session.verify = False # SSL verification turned off, requests will give an InsecureRequestWarning
 ticker = "TSLA"
 source = "yahoo-dividends"
 today = dt.date.today()
 
 try:
-    df = web.DataReader(ticker,source,today)
+    df = web.DataReader(ticker,source,today,session=session)
     print(df.head())
 except KeyError as err:
     print("{}, market may not be open.".format(err))
